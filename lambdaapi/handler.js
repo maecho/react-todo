47f997e1e7fa397cbe35665f6bcfa578589564d0
gcmae <k.maeda@gemcook.com>
2017-03-21 01:30:21 +0900

    v1.1 localStorage

--- lambdaapi/handler.js
+++ lambdaapi/handler.js
@@ -1,16 +1,32 @@
-'use strict';
+var AWS = require("aws-sdk");
+var dynamo = new AWS.DynamoDB.DocumentClient();
+module.exports.todosReadAll = function(event, context) {
+    dynamo.scan({TableName : "playground-gcmae"}, function(err, data) {
+        if (err) {
+            context.fail(err); // エラー時
+        } else {
+            context.succeed(data); // 正常時
+        }
+    });
+};
 
-module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
+module.exports.update = (event, context, callback) => {
+  const id = event.pathParameters.id,
+        data = JSON.parse(event.body);
+
+  const params = {
+    TableName : 'playground-gcmae',
+    Item: {
+      id,
+      data
+    }
   };
 
-  callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+  return db.put(params, (err, data) => {
+    if (err) {
+      callback(err);
+    } else {
+      callback(err, params.Item);
+    }
+  });
 };
